9dd54694cff83a7d88d7ef2c13f2d05064a5d7a5
Jessada Supapsuntorn <26509248+lzy3me@users.noreply.github.com>
2022-11-07 01:54:35 +0700

    test run script

--- src/webhook/webhook.service.ts
+++ src/webhook/webhook.service.ts
@@ -2,25 +2,39 @@ import { Injectable } from "@nestjs/common";
 import { Client as LineClient } from "@line/bot-sdk";
 import ReceiveWebhookDto from "./dto/receive-webhook.dto";
 import { ConfigService } from "@nestjs/config";
+import { spawn } from "node:child_process";
 
 @Injectable()
 export class WebhookService {
-  constructor(
-    private configService: ConfigService,
-  ) {
+  constructor(private configService: ConfigService) {
     this.configService.get<string>("configuration.channelSecret");
   }
 
   receiving(body: ReceiveWebhookDto) {
     console.log(body);
     const client = new LineClient({
-      channelAccessToken: this.configService.get<string>("configuration.channelSecret"),
+      channelAccessToken: this.configService.get<string>(
+        "configuration.channelSecret",
+      ),
     });
 
     client.replyMessage(body.events.replyToken, {
       type: "text",
       text: "##DEPLOY##",
     });
+
+    const ls = spawn("ls", ["-lh", "/usr"]);
+    ls.stdout.on("data", (data) => {
+      console.log("stdout: ", data);
+    });
+
+    ls.stderr.on("data", (data) => {
+      console.error("stderr: ", data);
+    });
+
+    ls.on("close", (code) => {
+      console.log("child process exited with code ", code);
+    })
 
     return "okay";
   }
